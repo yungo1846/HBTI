ce0b2cf337f7371b65e2952fb8e57a463be0b6ae
yungo1846 <yungo1468@naver.com>
2021-01-18 23:43:25 +0900

    fix: environ

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('secret_key')
+SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 Local = False
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.180.83.176']
+ALLOWED_HOSTS = ['54.180.83.176', '127.0.0.1']
 
 
 # Application definition
@@ -97,7 +97,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'hbti',
             'USER': 'yungo1846',
-            'PASSWORD': os.getenv('server_db_password'),
+            'PASSWORD': os.environ['server_db_password'],
             'HOST': 'hbti.cblipspnkvrs.ap-northeast-2.rds.amazonaws.com',
             'PORT': '5432'
         }
