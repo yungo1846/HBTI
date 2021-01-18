4dc8d545847bbe3d3ec9a1780aaedf86b652d7ba
yungo1846 <yungo1468@naver.com>
2021-01-18 14:47:06 +0900

    fix: allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.79.250.59']
 
 
 # Application definition
