466c7de6197495a4c5fe5a5903c245505b3b85de
yungo1846 <yungo1468@naver.com>
2021-01-19 22:47:25 +0900

    fix: scroll capture

--- frontend/src/components/HBTItestResult.js
+++ frontend/src/components/HBTItestResult.js
@@ -76,19 +76,15 @@ const HBTItestResult = ({ submittedList, userInfos }) => {
     newLogo.id = "logo";
     newLogo.innerHTML = `<img src="/image/logo.png" alt="logo" />`;
     const vp = document.getElementById("viewportMeta").getAttribute("content");
-    const newResultBox = new Promise(function (resolve, reject) {
-      document.getElementById("result-box").insertAdjacentElement("afterbegin", newLogo);
-      document.getElementById("viewportMeta").setAttribute("content", "width=800");
-      setTimeout(function () {
-        window.scrollTo(0, 0);
-      }, 100);
-    });
-    newResultBox
-      .then(
-        html2canvas(document.getElementById("result-box")).then((canvas) => {
-          saveAs(canvas.toDataURL("image/png", 1), "hbti-result.png");
-        })
-      )
+    document.getElementById("result-box").insertAdjacentElement("afterbegin", newLogo);
+    document.getElementById("viewportMeta").setAttribute("content", "width=800");
+    html2canvas(document.getElementById("result-box"), {
+      scrollX: 0,
+      scrollY: -window.scrollY,
+    })
+      .then((canvas) => {
+        saveAs(canvas.toDataURL("image/png", 1), "hbti-result.png");
+      })
       .then(document.getElementById("result-box").removeChild(newLogo))
       .then(document.getElementById("viewportMeta").setAttribute("content", vp));
   }
